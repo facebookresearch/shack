7287cc34ae6470692fd1391b896749235d24c6c7
Vincent Siles <vsiles@fb.com>
2022-01-21 17:54:45 +0100

    Bundle persistent in the interp type
    `interp` is now a record of a function from values to iProp, and
    a proof of persistence. This will make future work with type environment
    and generic eaiser and more resilient

--- theories/interp.v
+++ theories/interp.v
@@ -12,6 +12,53 @@ From iris.proofmode Require Import tactics.
 
 From shack Require Import lang heap modality.
 
+(* the interpretation of types is simply given by
+     the carrier set of the sem_typeO ofe *)
+Record interp Σ := Interp {
+    interp_car :> value → iPropO Σ;
+    interp_persistent v : Persistent (interp_car v)
+  }.
+
+Lemma interp_car_unfold : ∀ Σ c p, @interp_car Σ (Interp _ c p) = c.
+Proof.
+  by intros.
+Qed.
+
+Arguments Interp {_} _%I {_}.
+Arguments interp_car {_} _ _ : simpl never.
+
+Global Existing Instance interp_persistent.
+
+Section interp_cofe.
+  Context {Σ : gFunctors}.
+
+  Instance interp_equiv : Equiv (interp Σ) := λ A B, ∀ w, A w ≡ B w.
+  Instance interp_dist : Dist (interp Σ) := λ n A B, ∀ w, A w ≡{n}≡ B w.
+
+  Lemma interp_ofe_mixin : OfeMixin (interp Σ).
+  Proof. by apply (iso_ofe_mixin (interp_car : _ → value -d> _)). Qed.
+
+  Canonical Structure interpO := Ofe (interp Σ) interp_ofe_mixin.
+  Global Instance interp_cofe : Cofe interpO.
+  Proof.
+    apply (iso_cofe_subtype' (λ A : value -d> iPropO Σ, ∀ w, Persistent (A w))
+      (@Interp _) interp_car)=> //.
+    - apply _.
+    - apply limit_preserving_forall=> w.
+      by apply bi.limit_preserving_Persistent=> n ??.
+  Qed.
+
+  Global Instance interp_inhabited : Inhabited (interp Σ) := populate (Interp inhabitant).
+
+  Global Instance interp_car_ne n : Proper (dist n ==> (=) ==> dist n) interp_car.
+  Proof. by intros A A' ? w ? <-. Qed.
+
+  Global Instance interp_car_proper : Proper ((≡) ==> (=) ==> (≡)) interp_car.
+  Proof. by intros A A' ? w ? <-. Qed.
+End interp_cofe.
+
+Arguments interpO : clear implicits.
+
 Section proofs.
   (* assume a given set of class definitions *)
   Context `{PDC: ProgDefContext}.
@@ -19,47 +66,41 @@ Section proofs.
   (* Iris semantic context *)
   Context `{!sem_heapGS Σ}.
 
-  Notation iProp := (iProp Σ).
   Notation γ := sem_heap_name.
 
   (* Helping the inference with this notation that hides Δ *)
   Local Notation "s <: t" := (@subtype _ s t) (at level 70, no associativity).
   Local Notation "lty <:< rty" := (@lty_sub _ lty rty) (at level 70, no associativity).
 
-  (* the interpretation of types is simply given by
-     the carrier set of the sem_typeO ofe *)
-  Notation interpO := (sem_typeO Σ).
-  Definition interp := ofe_car interpO.
-  Eval hnf in interp.
-  (* = value → iPropO Σ *)
-  (*      : Type *)
-
   (* now, let's interpret some types ! *)
+  Definition interp_null_car (v: value) : iProp Σ := ⌜v = NullV⌝%I.
+  Definition interp_null_persistent v : Persistent (interp_null_car v).
+  Proof. by apply _. Qed.
 
-  Definition interp_null : interp :=
-    λ (v : value), ⌜v = NullV⌝%I.
+  Definition interp_null : interp Σ :=
+    Interp (λ(v: value), ⌜v = NullV⌝%I).
 
-  Definition interp_int : interp :=
-    λ (v : value), (∃ z, ⌜v = IntV z⌝)%I.
+  Definition interp_int : interp Σ :=
+    Interp (λ (v : value), (∃ z, ⌜v = IntV z⌝)%I).
 
-  Definition interp_bool : interp :=
-    λ (v : value), (∃ b, ⌜v = BoolV b⌝)%I.
+  Definition interp_bool : interp Σ :=
+    Interp (λ (v : value), (∃ b, ⌜v = BoolV b⌝)%I).
 
-  Definition interp_union (iA : interp) (iB : interp) : interp :=
-    λ (v : value), (iA v ∨ iB v)%I.
+  Definition interp_union (iA : interp Σ) (iB : interp Σ) : interp Σ :=
+    Interp (λ (v : value), (iA v ∨ iB v)%I).
 
-  Definition interp_inter (iA : interp) (iB : interp) : interp :=
-    λ (v : value), (iA v ∧ iB v)%I.
+  Definition interp_inter (iA : interp Σ) (iB : interp Σ) : interp Σ :=
+    Interp (λ (v : value), (iA v ∧ iB v)%I).
 
-  Definition interp_nothing : interp :=
-    λ (_: value), False%I.
+  Definition interp_nothing : interp Σ :=
+    Interp (λ (_: value), False%I).
 
-  Notation ty_interpO := (lang_ty -d> interpO).
+  Notation ty_interpO := (lang_ty -d> sem_typeO Σ).
 
   Definition interp_fields (rec: ty_interpO) (ftys: stringmap lang_ty) :
-    gmapO string (laterO interpO) :=
+    gmapO string (laterO (sem_typeO Σ)) :=
     let f := λ (typ: lang_ty), Next (rec typ) in
-    @fmap _ _ _ (later interpO) f ftys
+    @fmap _ _ _ (later (sem_typeO Σ)) f ftys
   .
 
   Lemma interp_fields_contractive ftys:
@@ -74,21 +115,25 @@ Section proofs.
 
   (* interpret a class type given the tag and the
      interpretation of their fields *)
-  Definition interp_class (cname : tag) (rec : ty_interpO) : interp :=
+  Definition interp_class_car (cname : tag) (rec : ty_interpO) : ofe_car (sem_typeO Σ) :=
     let f := λ (typ: lang_ty), Next (rec typ) in
     λ (w : value),
     (∃ ℓ t (fields:stringmap lang_ty),
     ⌜w = LocV ℓ ∧ inherits t cname ∧ has_fields t fields⌝ ∗
     (ℓ ↦ (t , interp_fields rec fields)))%I.
 
-  Definition interp_nonnull (rec : ty_interpO) : interp :=
+  Definition interp_class (cname: tag) (rec: ty_interpO): interp Σ :=
+    Interp (interp_class_car cname rec).
+
+  Definition interp_nonnull (rec : ty_interpO) : interp Σ :=
+    Interp (
     λ (v : value),
     ((interp_int v) ∨
     (interp_bool v) ∨
-    (∃ t, interp_class t rec v))%I.
+    (∃ t, interp_class t rec v))%I).
 
-  Definition interp_mixed (rec: ty_interpO) : interp :=
-    λ (v: value), (interp_nonnull rec v ∨ interp_null v)%I.
+  Definition interp_mixed (rec: ty_interpO) : interp Σ :=
+    Interp (λ (v: value), (interp_nonnull rec v ∨ interp_null v)%I).
 
   (* we use a blend of Coq/Iris recursion, the
      Coq recursion lets us handle simple structural
@@ -96,7 +141,7 @@ Section proofs.
      the more complicated case of class types *)
   Definition interp_type_pre (rec : ty_interpO) : ty_interpO :=
     λ (typ : lang_ty),
-    (fix go (typ : lang_ty) : interp :=
+    (fix go (typ : lang_ty) : interp Σ :=
     match typ with
     | IntT => interp_int
     | BoolT => interp_bool
@@ -109,12 +154,17 @@ Section proofs.
     | InterT A B => interp_inter (go A) (go B)
     end) typ.
 
-  Lemma interp_class_contractive cname: Contractive (interp_class cname). 
+  Lemma interp_class_contractive cname:
+    Contractive (interp_class cname).
   Proof.
     rewrite /interp_class => n i1 i2 hdist v.
-    do 3 (f_equiv; intro).
+    rewrite !interp_car_unfold.
+    rewrite /interp_class_car.
+    f_equiv ; intro l.
+    f_equiv; intro t.
+    f_equiv; intro fields.
     f_equiv.
-    set (f := λ i, interp_fields i a1).
+    set (f := λ i, interp_fields i fields).
     assert (hf : Contractive f) by apply interp_fields_contractive.
     by apply (mapsto_contractive _ _ f).
   Qed.
@@ -122,6 +172,7 @@ Section proofs.
   Lemma interp_nonnull_contractive: Contractive interp_nonnull.
   Proof.
     rewrite /interp_nonnull => n i1 i2 hdist v.
+    rewrite !interp_car_unfold.
     do 4 f_equiv.
     revert v.
     by apply interp_class_contractive.
@@ -135,12 +186,12 @@ Section proofs.
   Proof.
     move => n i1 i2 hdist.
     move => ty.
-    elim : ty; intros => //=;
+    elim : ty; intros; rewrite /interp_type_pre //=;
     [ (* mixed *)| (*class*) | (*nonnull*) 
- | solve_proper_core ltac:(fun _ => first [done | f_contractive | f_equiv])
- | solve_proper_core ltac:(fun _ => first [done | f_contractive | f_equiv])
-     ].
-     - move => v; rewrite /interp_mixed.
+    | solve_proper_core ltac:(fun _ => first [done | f_contractive | f_equiv])
+    | solve_proper_core ltac:(fun _ => first [done | f_contractive | f_equiv])
+    ].
+     - move => v; rewrite /interp_mixed !interp_car_unfold.
        f_equiv; revert v.
        by apply interp_nonnull_contractive.
      - by apply interp_class_contractive.
@@ -164,10 +215,6 @@ Section proofs.
   Proof.
     elim => [ | | | | cname | | |s hs t ht | s hs t ht] v;
         rewrite interp_type_unfold //=; try by apply _.
-    - rewrite /interp_union.
-      by apply bi.or_persistent; rewrite -!interp_type_unfold.
-    - rewrite /interp_union.
-      by apply bi.and_persistent; rewrite -!interp_type_unfold.
   Qed.
 
   Lemma dom_interp_fields fields:
@@ -261,7 +308,7 @@ Section proofs.
   Qed.
 
   Definition interp_local_tys
-    (lty : local_tys) (le : local_env) : iProp :=
+    (lty : local_tys) (le : local_env) : iProp Σ :=
     (∀ v ty, ⌜lty !! v = Some ty⌝ -∗
     ∃ val, ⌜le !! v = Some val⌝ ∗ interp_type ty val)%I.
 
