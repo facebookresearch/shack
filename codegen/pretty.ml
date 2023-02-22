838ce0e7a507d875ac7eda42ad157bc008cdce97
Vincent Siles <vsiles@meta.com>
2023-02-22 10:54:22 +0100

    provided pp formatter for types

--- codegen/pretty.ml
+++ codegen/pretty.ml
@@ -6,73 +6,88 @@
  *)
 
 open Ast
-open Printf
 
-let rec lang_ty_pretty = function
-  | IntT -> "IntT"
-  | BoolT -> "BoolT"
-  | NothingT -> "NothingT"
-  | MixedT -> "MixedT"
+let fmt_kw ppf s = Format.fprintf ppf "%s" s
+let fmt_str ppf s = Format.fprintf ppf "\"%s\"" s
+
+let fmt_list fa ppf xs =
+  let semi ppf () = Format.fprintf ppf "@ ; " in
+  Format.fprintf ppf "@[<v>[%a]@]" (Format.pp_print_list ~pp_sep:semi fa) xs
+
+let rec fmt_ty ppf = function
+  | IntT -> Format.fprintf ppf "%a" fmt_kw "IntT"
+  | BoolT -> Format.fprintf ppf "%a" fmt_kw "BoolT"
+  | NothingT -> Format.fprintf ppf "%a" fmt_kw "NothingT"
+  | MixedT -> Format.fprintf ppf "%a" fmt_kw "MixedT"
   | ClassT { name; tyargs } ->
-      let l = List.map lang_ty_pretty tyargs in
-      let s = String.concat ";" l in
-      sprintf "(ClassT true \"%s\" ([%s]))" name s
-  | NullT -> "NullT"
-  | NonNullT -> "NonNullT"
+      Format.fprintf ppf "@[ClassT@ ";
+      Format.fprintf ppf "true@ ";
+      Format.fprintf ppf "%a@ " fmt_str name;
+      Format.fprintf ppf "%a@]" (fmt_list fmt_ty) tyargs
+  | NullT -> Format.fprintf ppf "%a" fmt_kw "NullT"
+  | NonNullT -> Format.fprintf ppf "%a" fmt_kw "NonNullT"
   | UnionT (s, t) ->
-      sprintf "(UnionT (%s) (%s))" (lang_ty_pretty s) (lang_ty_pretty t)
+      Format.fprintf ppf "@[UnionT@ (@[%a@])@ (@[%a@])@]" fmt_ty s fmt_ty t
   | InterT (s, t) ->
-      sprintf "(InterT (%s) (%s))" (lang_ty_pretty s) (lang_ty_pretty t)
-  | GenT _ -> (* TODO: debruijn ? *) "(GenT XXX)"
-  | DynamicT -> "DynamicT"
-  | SupportDynT -> "SupportDynT"
+      Format.fprintf ppf "@[InterT@ (@[%a@])@ (@[%a@])@]" fmt_ty s fmt_ty t
+  | GenT s ->
+      (* TODO deBruijn *)
+      Format.fprintf ppf "@[GenT@ %a@]" fmt_str s
+  | DynamicT -> Format.fprintf ppf "%a" fmt_kw "DynamicT"
+  | SupportDynT -> Format.fprintf ppf "%a" fmt_kw "SupportDynT"
+
+(* TODO remove *)
+let lang_ty_pretty ty = Format.asprintf "%a" fmt_ty ty
 
 let rec expr_pretty = function
-  | IntE n -> sprintf "(IntE %d)" n
-  | BoolE b -> sprintf "(BoolE %b)" b
+  | IntE n -> Printf.sprintf "IntE %d" n
+  | BoolE b -> Printf.sprintf "BoolE %b" b
   | NullE -> "NullE"
   | BinOpE { op; lhs; rhs } ->
-      sprintf "(BinOpE %s (%s) (%s))" (show_binop op) (expr_pretty lhs)
+      Printf.sprintf "BinOpE %s (%s) (%s)" (show_binop op) (expr_pretty lhs)
         (expr_pretty rhs)
   | UniOpE { op; e } ->
-      sprintf "(UniOpE %s (%s))" (show_uniop op) (expr_pretty e)
-  | VarE s -> sprintf "(VarE %s)" s
+      Printf.sprintf "UniOpE %s (%s)" (show_uniop op) (expr_pretty e)
+  | VarE s -> Printf.sprintf "VarE %s" s
   | ThisE -> "ThisE"
   | UpcastE { e; ty } ->
-      sprintf "(Upcast (%s) (%s))" (expr_pretty e) (lang_ty_pretty ty)
+      Printf.sprintf "Upcast (%s) (%s)" (expr_pretty e) (lang_ty_pretty ty)
 
 let runtime_check_pretty = function
-  | RCTag s -> sprintf "(RCTag \"%s\")" s
+  | RCTag s -> Printf.sprintf "RCTag \"%s\"" s
   | RCInt -> "RCInt"
   | RCBool -> "RCBool"
   | RCNull -> "RCNull"
   | RCNonNull -> "RCNonNull"
 
 let pretty_map p xs =
-  let xs = List.map (fun (key, v) -> sprintf "\"%s\" := %s" key (p v)) xs in
+  let xs =
+    List.map (fun (key, v) -> Printf.sprintf "\"%s\" := %s" key (p v)) xs
+  in
   match xs with
   | [] -> "∅"
   | _ ->
       let s = String.concat ";" xs in
-      sprintf "{[ %s ]}" s
+      Printf.sprintf "{[ %s ]}" s
 
 let rec cmd_pretty = function
   | SkipC -> "SkipC"
   | SeqC { fstc; sndc } ->
-      sprintf "(SeqC (%s) (%s))" (cmd_pretty fstc) (cmd_pretty sndc)
-  | LetC { lhs; e } -> sprintf "(LetC \"%s\" (%s))" lhs (expr_pretty e)
+      Printf.sprintf "SeqC (%s) (%s)" (cmd_pretty fstc) (cmd_pretty sndc)
+  | LetC { lhs; e } -> Printf.sprintf "LetC \"%s\" (%s)" lhs (expr_pretty e)
   | IfC { cond; thn; els } ->
-      sprintf "(IfC (%s) (%s) (%s))" (expr_pretty cond) (cmd_pretty thn)
+      Printf.sprintf "IfC (%s) (%s) (%s)" (expr_pretty cond) (cmd_pretty thn)
         (cmd_pretty els)
   | GetC { lhs; recv; name } ->
-      sprintf "(GetC \"%s\" (%s) \"%s\")" lhs (expr_pretty recv) name
+      Printf.sprintf "GetC \"%s\" (%s) \"%s\"" lhs (expr_pretty recv) name
   | SetC { recv; name; rhs } ->
-      sprintf "(SetC (%s) \"%s\" (%s))" (expr_pretty recv) name
+      Printf.sprintf "SetC (%s) \"%s\" (%s)" (expr_pretty recv) name
         (expr_pretty rhs)
   | ErrorC -> "ErrorC"
   | CallC { lhs; recv; name; args } ->
       let args = pretty_map expr_pretty args in
-      sprintf "(CallC \"%s\" (%s) \"%s\" %s)" lhs (expr_pretty recv) name args
+      Printf.sprintf "CallC \"%s\" (%s) \"%s\" %s" lhs (expr_pretty recv) name
+        args
   | NewC { lhs; name; ty_args; args } ->
       let args = pretty_map expr_pretty args in
       let ty_args =
@@ -81,9 +96,9 @@ let rec cmd_pretty = function
         | _ ->
             let l = List.map lang_ty_pretty ty_args in
             let s = String.concat ";" l in
-            sprintf "(Some [ %s ])" s
+            Printf.sprintf "(Some [ %s ])" s
       in
-      sprintf "(NewC \"%s\" \"%s\" %s %s)" lhs name ty_args args
+      Printf.sprintf "NewC \"%s\" \"%s\" %s %s" lhs name ty_args args
   | RuntimeCheckC { v; rc; thn; els } ->
       ignore v;
       ignore rc;
@@ -96,17 +111,17 @@ let methodDef_pretty cname { name; args; return_type; body; return } =
   let l =
     [
       "{|";
-      sprintf "  methodargs := %s;" args;
-      sprintf "  methodrettype := %s;" (lang_ty_pretty return_type);
-      sprintf "  methodbody := %s;" (cmd_pretty body);
-      sprintf "  methodret := %s;" (expr_pretty return);
-      sprintf "  methodvisibility := Public;";
+      Printf.sprintf "  methodargs := %s;" args;
+      Printf.sprintf "  methodrettype := %s;" (lang_ty_pretty return_type);
+      Printf.sprintf "  methodbody := %s;" (cmd_pretty body);
+      Printf.sprintf "  methodret := %s;" (expr_pretty return);
+      Printf.sprintf "  methodvisibility := Public;";
       (* TODO *)
       "|}";
     ]
   in
   let s = String.concat "\n" l in
-  sprintf "Definition %s_%s := %s." cname name s
+  Printf.sprintf "Definition %s_%s := %s." cname name s
 
 let classDef_pretty { name; generics = _; constraints; super; fields; methods }
     =
@@ -122,20 +137,20 @@ let classDef_pretty { name; generics = _; constraints; super; fields; methods }
            | Super -> [ (r, l) ])
          constraints
   in
-  let cs = List.map (fun (l, r) -> sprintf "(%s, %s)" l r) cs in
-  let cs = sprintf "[%s]" (String.concat ";" cs) in
+  let cs = List.map (fun (l, r) -> Printf.sprintf "(%s, %s)" l r) cs in
+  let cs = Printf.sprintf "[%s]" (String.concat ";" cs) in
   let super =
     match super with
     | None -> "None"
     | Some (t, targs) ->
         let l = List.map lang_ty_pretty targs in
         let s = String.concat ";" l in
-        sprintf "Some(\"%s\", [%s])" t s
+        Printf.sprintf "Some(\"%s\", [%s])" t s
   in
   let fields =
     List.map
       (fun (vis, ty, name) ->
-        sprintf "\"%s\" := (%s, %s)" name (show_visibility vis)
+        Printf.sprintf "\"%s\" := (%s, %s)" name (show_visibility vis)
           (lang_ty_pretty ty))
       fields
   in
@@ -144,24 +159,24 @@ let classDef_pretty { name; generics = _; constraints; super; fields; methods }
     | [] -> "∅"
     | _ ->
         let s = String.concat ";" fields in
-        sprintf "{[%s]}" s
+        Printf.sprintf "{[%s]}" s
   in
   let l =
     [
       "{|";
-      sprintf "  superclass := %s;" super;
-      sprintf "  generics := [];";
-      sprintf "  constraints := %s;" cs;
-      sprintf "  classfields := %s;" fields;
-      sprintf "  classmethods := ∅;";
+      Printf.sprintf "  superclass := %s;" super;
+      Printf.sprintf "  generics := [];";
+      Printf.sprintf "  constraints := %s;" cs;
+      Printf.sprintf "  classfields := %s;" fields;
+      Printf.sprintf "  classmethods := ∅;";
       "|}";
     ]
   in
   let s = String.concat "\n" l in
-  let cls = sprintf "Definition %s := %s." name s in
+  let cls = Printf.sprintf "Definition %s := %s." name s in
   let mthds = List.map (fun mdef -> methodDef_pretty name mdef) methods in
   let mthds = String.concat "\n\n" mthds in
-  sprintf "%s\n\n%s" mthds cls
+  Printf.sprintf "%s\n\n%s" mthds cls
 
 let program_pretty l =
   let prelude = [ "Definition arraykey := UnionT IntT BoolT." ] in
