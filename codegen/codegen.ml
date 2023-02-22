9894e22eb96edc119757578ad6a5cc9f88ac500e
Vincent Siles <vsiles@meta.com>
2023-02-22 15:32:07 +0100

    [codegen] add a proper CLI interface.
    Also update main.yml who was probably missing menhir

--- codegen/codegen.ml
+++ codegen/codegen.ml
@@ -8,17 +8,56 @@
 open Core
 open Errors
 
-let process filename =
+let process_cmd input_file output_file =
   try
-    let inx = In_channel.create filename in
+    let inx = In_channel.create input_file in
     let lexbuf = Lexing.from_channel inx in
     let raw_term = Parser.prog Lexer.lexer lexbuf in
     let () = In_channel.close inx in
-    let () = Printf.printf "%s\n" @@ Pretty.program_pretty raw_term in
-    (* let () = Printf.printf "%s\n" @@ Ast.show_program raw_term in *)
+    let s = Pretty.program_pretty raw_term in
+    let () =
+      match output_file with
+      | None -> Printf.printf "%s\n" s
+      | Some output_file ->
+          let outx = Out_channel.create output_file in
+          let () = Printf.fprintf outx "%s\n" s in
+          let () = Out_channel.close outx in
+          ()
+    in
     ()
   with
   | TError (e, l, m) -> printf "Error: %s\n" (msg_of_error e l m)
   | Sys_error s -> printf "System Error: %s\n" s
 
-let () = process "./test.lang"
+let env =
+  let open Cmdliner in
+  let input_file =
+    let doc = "path to an input file." in
+    Arg.(
+      required & opt (some string) None & info [ "f"; "file" ] ~doc ~docv:"FILE")
+  in
+  let output_file =
+    let doc = "path to an output file. Will print on stdout if not provided." in
+    Arg.(
+      value
+      & opt (some string) None
+      & info [ "o"; "output" ] ~doc ~docv:"OUTPUT")
+  in
+  Term.(const process_cmd $ input_file $ output_file)
+
+let main () =
+  let open Cmdliner in
+  let cmd =
+    let doc = "parse Hack-like files and output Coq content for Shack." in
+    let man =
+      [
+        `S Manpage.s_description;
+        `P (String.strip {| Process Hack-like files for Shack." |});
+      ]
+    in
+    let info = Cmd.info "codegen" ~version:"%%VERSION%%" ~doc ~man in
+    Cmd.v info env
+  in
+  exit (Cmd.eval cmd)
+
+let () = main ()
