d2d3385b568e847ec72541968dc051484e65a259
Vincent Siles <vsiles@meta.com>
2023-02-23 19:24:33 +0100

    adding missing headers

--- codegen/location.ml
+++ codegen/location.ml
@@ -1,3 +1,10 @@
+(*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *)
+
 type t = { startpos : Lexing.position; endpos : Lexing.position }
 
 let dummy = { startpos = Lexing.dummy_pos; endpos = Lexing.dummy_pos }
